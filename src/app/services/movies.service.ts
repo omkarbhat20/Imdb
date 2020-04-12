b3d4f3ddffd2ab1d84a126e0c55c1a1f7b0d56cc
Akshay <akshay.mhatre8989@gmail.com>
2020-04-12 15:53:43 +0530

    Changes: 1. Movies service updated with error handling 2. Interfaces added for movies model. 3. JsDoc commenting added for services methods.

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,19 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Movies } from '../movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  private movieSearchUrl = 'http://www.omdbapi.com/?apikey=81e855f&s=';
+  private getMovieByIdUrl = 'http://www.omdbapi.com/?apikey=81e855f&i=';
 
   constructor(private http: HttpClient) { }
-  movieSearch(title) {
-    const url = 'http://www.omdbapi.com/?apikey=81e855f&s=' + title;
-    return this.http.get(url);
+
+  /**
+   * Movies search by title
+   * @param title string that will include movie title
+   * @returns Observable of type Http Response
+   */
+  movieSearch(title: string) {
+    return this.http.get<Observable<Movies>>(this.movieSearchUrl + title)
+      .pipe(map((movies: Observable<Movies>) => {
+        return movies ? movies : ''
+      }));
   }
-  getMovieById(id){
-    const url = 'http://www.omdbapi.com/?apikey=81e855f&i=' + id;
-    return this.http.get(url);
+
+  /**
+   * Gets movie by id
+   * @param id number that will be movie id
+   * @returns Observable of type Http Response
+   */
+  getMovieById(id: number) {
+    return this.http.get<Observable<Movies>>(this.getMovieByIdUrl + id)
+    .pipe(map((movies: Observable<Movies>) => {
+      return movies ? movies : ''
+    }));
   }
+
 }
